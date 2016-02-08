da120fd6304f36a98a55e94bcc933dee144df1fc
Joern Hahn <joern.hahn@gmail.com>
2016-02-08 21:46:57 +0100

    second commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -94,6 +94,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
+      
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
@@ -120,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/static/'
+ 
